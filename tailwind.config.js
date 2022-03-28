9a8efa6ad6fa5fdafa7a4246b58190b47fe9ba37
Maximiliano Sánchez <72763554+maxisan@users.noreply.github.com>
2022-03-28 16:48:39 -0300

    Revert "Added custom fonts to global style, configurated tailwind theme"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Quicksand', ...defaultTheme.fontFamily.sans],
-        'mono': ['"Share Tech Mono"', ...defaultTheme.fontFamily.mono]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
