77fabda14831de4afcb08500b4a579bbc83456af
Maxisan <maxisanchez19@gmail.com>
2022-09-30 15:16:33 -0300

    Created animation with gsap

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import OpenIcon from 'public/assets/open.svg';
 import CloseIcon from 'public/assets/close.svg';
 import {LoadingContainer, LogoContainer, TextContainer} from './styles';
+import { gsap } from 'gsap';
 
 function Loading() {
+  const diamondRef = useRef(null);
+  const textRef = useRef(null)
+  const tl = useRef({})
+
+  useEffect(() => {
+    const rotation = {
+      rotation: "+=90",
+      duration: 0.7,
+      delay: 0.3,
+      ease: 'expo.inOut'
+    }
+
+    tl.current = gsap.timeline()
+      .to(diamondRef.current, rotation)
+      .to(diamondRef.current, rotation)
+      .to(diamondRef.current, rotation)
+      .to(diamondRef.current, rotation)
+      .to(textRef.current, {
+        duration: 1.2,
+        width: "+=3.5em",
+        ease: 'expo.inOut'
+      })
+      .to(textRef.current, {
+        width: "-=2.7em",
+        delay: 0.7
+      })
+  })
+
   return (
     <LoadingContainer>
-      <LogoContainer>
+      <LogoContainer ref={diamondRef}>
         <Image src={OpenIcon} alt='loading' layout='fixed'/>
-        <TextContainer>
+        <TextContainer ref={textRef}>
           maxisan
         </TextContainer>
         <Image src={CloseIcon} alt='loading' layout='fixed'/>
--- components/Loading/styles.tsx
+++ components/Loading/styles.tsx
@@ -1,57 +1,4 @@
-import styled, { keyframes } from 'styled-components';
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  12.5% {
-    transform: rotate(0deg);
-  }
-  25% {
-    transform: rotate(90deg);
-  }
-  37.5% {
-    transform: rotate(90deg);
-  }
-  50% {
-    transform: rotate(180deg);
-  }
-  62.5% {
-    transform: rotate(180deg);
-  }
-  75% {
-    transform: rotate(270deg);
-  }
-  87.5% {
-    transform: rotate(270deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`
-
-const openDiamond = keyframes`
-  0% {
-    width: 100px;
-  }
-  50% {
-    width: 300px;
-  }
-  100% {
-    width: 120px;
-  }
-`
-const TextAppear = keyframes`
-  0% {
-    width: 0px;
-  }
-  50% {
-    width: 100%;
-  }
-  100% {
-    width: 120px;
-  }
-`
+import styled from 'styled-components';
 
 export const LoadingContainer = styled.div`
   display: flex;
@@ -68,12 +15,7 @@ export const LogoContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  transition: all;
-  animation-name: ${rotateAnimation}, ${openDiamond};
-  animation-duration: 3s, 2s; 
-  animation-timing-function: ease-in-out;
-  animation-delay: 0ms, 3000ms;
-  flex-shrink: 0;
+  justify-content: space-between;
 `
 
 export const TextContainer = styled.div`
@@ -83,10 +25,5 @@ export const TextContainer = styled.div`
   width: 0px;
   margin: 0 0.5rem;
   overflow: hidden;
-  transition: all;
-  animation-name: ${TextAppear};
-  animation-duration: 2s; 
-  animation-timing-function: ease-in-out;
-  animation-delay: 3200ms;
 `
 
